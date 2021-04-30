5ca6a8acb505264744256526630ccb07320adfdf
Dulvin Witharane <dulvinw@gmail.com>
2021-05-01 01:10:21 +0530

    Added working list tags endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'core',
-    'user'
+    'user',
+    'recipe'
 ]
 
 MIDDLEWARE = [
---
+++ app/recipe/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    Manage tags in a database
+    """
+
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializers
+
+    def get_queryset(self):
+        """Return only the tags related to current auth user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
